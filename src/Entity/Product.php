9d50b1276eac8d2687a47547315540b73ebfacba
AdrRnv <adrian.rabian@gmail.com>
2024-12-09 09:46:53 +0100

    feat(All): user

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -8,7 +8,6 @@ use App\Repository\ProductRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
